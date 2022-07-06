0af88fec20ef2a0ec48edc9788ce417b1d496964
hsuhsusophie <hsuhsusophie@gmail.com>
2022-07-07 00:31:07 +0800

    change4

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
