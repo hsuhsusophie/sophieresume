a9bc747ab7bae5c6ca50a03f53fcb86a9a7bbcd7
Nash0430 <sihuai430@gmail.com>
2022-07-13 22:51:27 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -6,21 +6,21 @@ const nextConfig = {
 module.exports = nextConfig
 
 
-function ActiveLink({ children, href }) {
-  const router = useRouter()
-  const style = {
-    marginRight: 10,
-    color: router.asPath === href ? 'red' : 'black',
-  }
+// function ActiveLink({ children, href }) {
+//   const router = useRouter()
+//   const style = {
+//     marginRight: 10,
+//     color: router.asPath === href ? 'red' : 'black',
+//   }
 
-  const handleClick = (e) => {
-    e.preventDefault()
-    router.push(href)
-  }
+//   const handleClick = (e) => {
+//     e.preventDefault()
+//     router.push(href)
+//   }
 
-  return (
-    <a href={href} onClick={handleClick} style={style}>
-      {children}
-    </a>
-  )
-}
+//   return (
+//     <a href={href} onClick={handleClick} style={style}>
+//       {children}
+//     </a>
+//   )
+// }
